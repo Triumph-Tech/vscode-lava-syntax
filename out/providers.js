c0b83047b4565090390a9efd4ea3c6dc2d6b2dde
Garrett Johnson <nvisiondesign@gmail.com>
2021-10-20 22:40:38 -0700

    Fix providers

--- out/providers.js
+++ out/providers.js
@@ -70,10 +70,7 @@ const getFilterProvider = (documentSelector) => {
             let linePrefix = currentLine.substr(0, position.character);
             let lineSuffix = currentLine.substr(position.character);
             //let documentPrefix = document.getText(new vscode.Range(documentStart, position));
-            if (!utils.isInsideBrackets(currentLine, position.character) && !utils.isInsideObject(currentLine, position.character)) {
-                return;
-            }
-            if (!utils.isLavaFilterable.test(linePrefix)) {
+            if (!utils.isLavaFilterable.test(linePrefix) && !utils.isInsideBrackets(currentLine, position.character) && !utils.isInsideObject(currentLine, position.character)) {
                 return;
             }
             //const textUntilPosition = this.intellisense.utils.textUntilPosition(model, position);
